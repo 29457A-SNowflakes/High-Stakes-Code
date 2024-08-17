4c1301e1f39bf3b7422d7e8eee1511a18152c490
KillerDew <daniel.dew007@gmail.com>
2024-08-17 15:31:32 +0100

    Robot class skeleton

---
+++ Main-Project/include/usr/robot.h
@@ -0,0 +1,19 @@
+#include "api.h"
+#include "lemlib/api.hpp"
+#include "robodash/api.h"
+
+
+
+
+class Robot {
+    public:
+        class Auton {
+            public:
+                class Tuning {};
+            // ...
+        };
+        class Motors {};
+        class Pneumatics {};
+        class Actions {};
+        class Logging {};
+};
